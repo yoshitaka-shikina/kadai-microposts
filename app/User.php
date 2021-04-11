298688941f0213b46dec6aa46a1849e0bac79a03
yoshitaka.shikina <yoshitaka.shikina0216@gmail.com>
2021-04-11 10:01:31 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -121,11 +121,16 @@ class User extends Authenticatable
     }
     
     /**
-     * このユーザに関係するモデルの件数をロードする。
+     * このユーザとフォロー中ユーザの投稿に絞り込む。
      */
-    public function loadRelationshipCounts()
+    public function feed_microposts()
     {
-        $this->loadCount('microposts');
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
     }
     
     /**
